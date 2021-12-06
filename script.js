33e1b240ff2cf9ab81c8e56d552042f3ccd481fd
stfDeveloper <sstefancosmin@icloud.com>
2021-12-06 18:47:25 +0100

    Script+html finiti

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 let userInputOld= prompt("Quanti anni hai?");
 let userInputKm= prompt("Quanti kilometri devi percorrere?");
+
+let discountPerAge= userInputKm * 0.21;
+
+if(userInputOld <18){
+    discountPerAge= discountPerAge - discountPerAge * 0.2;
+} else if(userInputOld >65){
+    discountPerAge= discountPerAge - discountPerAge * 0.4;
+}
+
+
+
+document.writeln('TOTALE DA PAGARE :' + discountPerAge + '€' )
